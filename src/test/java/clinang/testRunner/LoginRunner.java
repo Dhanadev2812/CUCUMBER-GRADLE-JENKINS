92b135b7fc0d78989984b9eef285ce9d2f921f67
Dhanadev2812 <54659828+Dhanadev2812@users.noreply.github.com>
2020-12-01 15:05:52 +0530

    Update LoginRunner.java

--- src/test/java/clinang/testRunner/LoginRunner.java
+++ src/test/java/clinang/testRunner/LoginRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(plugin = {"pretty","html:cucumber-html-reports","json: cucumber-html-reports/cucumber.json" },
-features= {"src/test/resources/clinang_Feature_Patient/Patient_bookappointment.feature"},
+features= {"src/test/resources/clinang_Feature_Patient/ClinicAdmin_alogin.feature"},
 glue= {"clinang.webDriverUtils","clinang.stepDefs"},
 tags = {"@Create_new_appointment"},
 monochrome = true,
