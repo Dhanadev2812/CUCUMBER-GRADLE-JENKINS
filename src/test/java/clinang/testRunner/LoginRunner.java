6f4ec84cdcecb2139048ca69ae3628ec46d2ee67
Dhanadev2812 <54659828+Dhanadev2812@users.noreply.github.com>
2020-12-02 09:34:34 +0530

    Update LoginRunner.java

--- src/test/java/clinang/testRunner/LoginRunner.java
+++ src/test/java/clinang/testRunner/LoginRunner.java
@@ -1,16 +1,8 @@
-package clinang.testRunner;
-import org.junit.runner.RunWith;
+import cucumber.junit.Cucumber;
+	import org.junit.runner.RunWith;
 
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
+	@RunWith(Cucumber.class)
+	@Cucumber.Options(format = {"pretty", "html:target/cucumber", "json:target/cucumber.json"})
+	public class MyTest {
 
-@RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty","html:Report/JenkinsReport/cucumberReports","json:target/cucumber.json" },
-features= {"src/test/resources/clinang_Feature_Patient/ClinicAdmin_01_alogin.feature"},
-glue= {"clinang.webDriverUtils","clinang.stepDefs"},
-tags = {"@login_valid"},
-monochrome = true,
-strict=true)
-public class LoginRunner {
-	
-}
+	}
